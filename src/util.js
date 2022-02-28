9a17617596a305dbdf712b88ce9dc90037dcb981
GameDog9988 <alex@grattan.me>
2022-02-28 18:27:47 -0500

    feat: add string to slug function

--- src/util.js
+++ src/util.js
@@ -11,3 +11,25 @@ export function getNodeText(node) {
   if (node instanceof Array) return node.map(getNodeText).join("");
   if (typeof node === "object" && node) return getNodeText(node.props.children);
 }
+
+export function stringToSlug(str) {
+  str = str.replace(/^\s+|\s+$/g, ""); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to = "aaaaaaeeeeiiiioooouuuunc------";
+
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-") // collapse dashes
+    .replace(/^-+/, "") // trim - from start of text
+    .replace(/-+$/, ""); // trim - from end of text
+
+  return str;
+}
