7e0cd5a95f9117c2a2fcfa51c43ea589d3ce60d2
GameDog9988 <alex@grattan.me>
2022-03-28 11:07:38 -0400

    fix: add optional chaining for sticky state

--- src/components/useStickyState.js
+++ src/components/useStickyState.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function useStickyState(defaultValue, key) {
   const [value, setValue] = useState(() => {
     const windowGlobal = typeof window !== `undefined` && window;
-    const stickyValue = windowGlobal
+    const stickyValue = windowGlobal?.localStorage
       ? windowGlobal.localStorage.getItem(key)
       : null;
     return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
