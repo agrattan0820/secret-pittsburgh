980bfaf2877158dba6f72d346606a5e2a34b706d
GameDog9988 <alex@grattan.me>
2022-05-06 22:04:06 +0100

    fix: remove dark mode for image spinner

--- src/components/drawerImage.js
+++ src/components/drawerImage.js
@@ -12,7 +12,7 @@ function DrawerImage({ src, alt }) {
       <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
         <svg
           role="status"
-          class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-pitt-blue"
+          class="w-8 h-8 text-gray-200 animate-spin fill-pitt-blue"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
