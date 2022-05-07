fd2e3d6d4acc239355d27c68a67c86c90d5ae8c7
GameDog9988 <alex@grattan.me>
2022-05-07 20:45:38 +0100

    feat: place in schema

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -4,6 +4,7 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import homepage from "./homepage";
+import place from "./place";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,5 +12,5 @@ export default createSchema({
   name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([homepage]),
+  types: schemaTypes.concat([homepage, place]),
 });
