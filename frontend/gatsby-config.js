3338a3781a432679d6d197b47eab18cb2aa7c7e7
GameDog9988 <alex@grattan.me>
2022-05-08 09:56:29 +0100

    feat: add gatsby purgecss plugin

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -64,5 +64,16 @@ module.exports = {
         apiBase: `jsonapi`,
       },
     },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        // More options defined here https://purgecss.com/configuration.html#options
+      },
+    },
   ],
 };
