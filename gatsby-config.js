b5a17692d02582ff44f2275c2bb8083aaeb97c5d
GameDog9988 <alex@grattan.me>
2022-05-08 22:48:53 +0100

    feat: purge global as well

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        purgeOnly: ["antd/dist/antd.css"], // Purge only these files/folders
+        purgeOnly: ["antd/dist/antd.css", "global.css"], // Purge only these files/folders
         purgeCSSOptions: {
           safelist: [
             "a",
