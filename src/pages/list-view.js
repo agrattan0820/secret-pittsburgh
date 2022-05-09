bc93ce8229aa6b77c97cebe4be8e91fe55c535e2
GameDog9988 <alex@grattan.me>
2022-05-09 21:01:08 +0100

    fix: add mx-auto to `list-view` back-to-top

--- src/pages/list-view.js
+++ src/pages/list-view.js
@@ -108,7 +108,7 @@ const ListViewPage = ({ data }) => {
                 ))}
             </ul>
           </div>
-          <div className="max-w-3xl">
+          <div className="max-w-3xl mx-auto">
             <button
               onClick={() => scrollTo("#page-top")}
               className="flex items-center justify-center px-4 py-2 space-x-2 font-bold text-center text-black transition transform rounded shadow hover:text-black bg-slate-200 hover:scale-105"
